1fdfe226b4dc8357662e23ac9676e52f5df85f4d
tjNane <470812009@qq.com>
2021-07-17 11:52:29 +0800

    fix:code

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page1234</h1>
       <el-row>
   <el-button>默认按钮</el-button>
   <el-button type="primary">主要按钮</el-button>
